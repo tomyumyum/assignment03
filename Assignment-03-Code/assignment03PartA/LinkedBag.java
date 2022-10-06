ec86876626d695de771a203615ab3ec465ceac5f
tomyumyum <nunapanda2013@gmail.com>
2022-10-06 14:00:20 -0700

    LinkBag TEMP

--- Assignment-03-Code/assignment03PartA/LinkedBag.java
+++ Assignment-03-Code/assignment03PartA/LinkedBag.java
@@ -16,6 +16,7 @@ public final class LinkedBag<T> implements PrimaryDataStructureBagInterface<T> {
 
     private Node firstNode;
     private int numberOfEntries;
+    private int[] Arr;
 
     public LinkedBag() {
         firstNode = null;
@@ -24,7 +25,48 @@ public final class LinkedBag<T> implements PrimaryDataStructureBagInterface<T> {
 
     @Override
     public boolean removeAllOccurrences(T[][] entries) {
+        return true;
     }
+
+
+
+    @Override
+    public int getCurrentSize() {
+        return numberOfEntries;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numberOfEntries == 0;
+    }
+
+    @Override
+    public boolean add(T newEntry) {
+        Node newNode = new Node (newEntry);
+        newNode.next = firstNode;
+        firstNode = newNode;
+        return true;
+    }
+
+    public static int mode(int[][] arr) {
+        int[] oneDArr = new int[arr.length * arr.length];
+        return 0; //TODO
+    }
+
+    @Override 
+    public T[] toArray(){
+        T[] result = (T[]) new Object[numberOfEntries];
+
+        int index = 0;
+        Node currentNode = firstNode;
+        while((index < numberOfEntries) && (currentNode != null)) {
+            result[index] = currentNode.data;
+            index++;
+            currentNode = currentNode.next;
+        }
+    }
+
+
 
     private class Node {
         private T data;
