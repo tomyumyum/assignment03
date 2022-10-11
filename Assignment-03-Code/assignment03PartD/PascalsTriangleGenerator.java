6d264a42b943a897e3a6795fa6680e3907387294
tomyumyum <nunapanda2013@gmail.com>
2022-10-11 10:17:19 -0700

    done

--- Assignment-03-Code/assignment03PartD/PascalsTriangleGenerator.java
+++ Assignment-03-Code/assignment03PartD/PascalsTriangleGenerator.java
@@ -17,5 +17,22 @@ public class PascalsTriangleGenerator {
     }
 
     public int[] computeRow(int rowToCompute) {
+        int[] value = new int[rowToCompute];
+
+        for (int i = 0; i < rowToCompute; i++) {
+            value[i] = pascalValue(rowToCompute -1,i);
+        }
+        return value;
+    }
+    private int pascalValue(int start, int end){
+        if (end == 0){
+            return 1;
+        }
+        else if (end == start) {
+            return 1;
+        }
+        else {
+            return pascalValue(-1,1) + pascalValue(start -1, end);
+        }
     }
 }
